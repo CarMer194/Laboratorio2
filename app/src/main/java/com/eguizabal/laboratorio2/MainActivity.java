1efd8639fb139f1100ed374237382ccadcf03bcb
CarMer194 <00060313@uca.edu.sv>
2018-03-22 21:46:30 -0600

    Primer intento

--- app/src/main/java/com/eguizabal/laboratorio2/MainActivity.java
+++ app/src/main/java/com/eguizabal/laboratorio2/MainActivity.java
@@ -7,13 +7,88 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView fstTxtView, sndTxtView;
+    private Juego juego;
+    private String auxString;
+    private TextView aux;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        juego = new Juego();
     }
 
-    
+    public void juegoJ1(View v){
+        Integer i;
+        i=juego.getSet();
+        juego.puntoJ1();
+        auxString = juego.getJuegoJ1();
+        if(i==1){
+            aux = findViewById(R.id.set1_j1);
+            aux.setText(auxString);
+        }
+        if(i==2){
+            aux = findViewById(R.id.set2_j1);
+            aux.setText(auxString);
+        }
+        if(i==3){
+            aux = findViewById(R.id.set3_j1);
+            aux.setText(auxString);
+        }
+        if(i==4){
+            aux = findViewById(R.id.set4_j1);
+            aux.setText(auxString);
+        }
+        if(i==5){
+            aux = findViewById(R.id.set5_j1);
+            aux.setText(auxString);
+        }
+        if(i==6){
+            aux = findViewById(R.id.set6_j1);
+            aux.setText(auxString);
+        }
+        if(i==7){
+            aux = findViewById(R.id.set7_j1);
+            aux.setText(auxString);
+        }
+    }
+
+    public void juegoJ2(View v){
+        Integer i;
+        i=juego.getSet();
+        juego.puntoJ1();
+        auxString = juego.getJuegoJ2();
+        if(i==1){
+            aux = findViewById(R.id.set1_j2);
+            aux.setText(auxString);
+        }
+        if(i==2){
+            aux = findViewById(R.id.set2_j2);
+            aux.setText(auxString);
+        }
+        if(i==3){
+            aux = findViewById(R.id.set3_j2);
+            aux.setText(auxString);
+        }
+        if(i==4){
+            aux = findViewById(R.id.set4_j2);
+            aux.setText(auxString);
+        }
+        if(i==5){
+            aux = findViewById(R.id.set5_j2);
+            aux.setText(auxString);
+        }
+        if(i==6){
+            aux = findViewById(R.id.set6_j2);
+            aux.setText(auxString);
+        }
+        if(i==7){
+            aux = findViewById(R.id.set7_j2);
+            aux.setText(auxString);
+        }
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/eguizabal/laboratorio2/juego.java
@@ -0,0 +1,120 @@
+package com.eguizabal.laboratorio2;
+
+/**
+ * Created by Carlos on 22/3/2018.
+ */
+
+class Juego {
+    private Integer set;
+    private String juegoJ1, juegoJ2;
+    private int contadorJ1=0, contadorJ2=0, juegoG1, juegoG2;
+    private boolean deuce;
+
+    public Juego() {
+        set = 1;
+        juegoJ1 = "-";
+        juegoJ2 = "-";
+        deuce=false;
+        juegoG1=0;
+        juegoG2=0;
+    }
+
+    public Integer getSet() {
+        return set;
+    }
+
+    public String getJuegoJ1() {
+        return juegoJ1;
+    }
+
+    public String getJuegoJ2() {
+        return juegoJ2;
+    }
+
+    public void setSet(Integer set) {
+        this.set = set;
+    }
+
+    public void setJuegoJ1(String juegoJ1) {
+        this.juegoJ1 = juegoJ1;
+    }
+
+    public void setJuegoJ2(String juegoJ2) {
+        this.juegoJ2 = juegoJ2;
+    }
+
+
+    public void puntoJ1(){
+        contadorJ1++;
+        if (contadorJ1==1){
+            juegoJ1 = "15";
+        }
+        if (contadorJ1==2){
+            juegoJ1 = "30";
+        }
+        if (contadorJ1==3){
+            juegoJ1 = "40";
+        }
+        if (contadorJ1==4 && contadorJ2==4){
+            deuce = true;
+            juegoJ1="40";
+        }
+        if (deuce && contadorJ1==5){
+            juegoJ1="AD";
+            juegoJ2="-";
+        }
+        if (contadorJ1==6 && deuce){
+            juegoJ1="-";
+            juegoJ2="-";
+            deuce=false;
+            set++;
+            juegoG1++;
+        }
+        if (contadorJ1==4 && !deuce){
+            juegoJ1="-";
+            juegoJ2="-";
+            contadorJ1=0;
+            set++;
+            juegoG1++;
+        }
+
+    }
+
+    public void puntoJ2(){
+        contadorJ2++;
+        if (contadorJ2==1){
+            juegoJ2 = "15";
+        }
+        if (contadorJ1==2){
+            juegoJ2 = "30";
+        }
+        if (contadorJ2==3){
+            juegoJ2 = "40";
+        }
+        if (contadorJ1==4 && contadorJ2==4){
+            deuce = true;
+            juegoJ2="40";
+        }
+        if (deuce && contadorJ2==5){
+            juegoJ2="AD";
+            juegoJ1="-";
+        }
+        if (contadorJ2==6 && deuce){
+            juegoJ1="-";
+            juegoJ2="-";
+            deuce=false;
+            set++;
+            juegoG2++;
+        }
+        if (contadorJ2==4 && !deuce){
+            juegoJ1="-";
+            juegoJ2="-";
+            contadorJ2=0;
+            set++;
+            juegoG2++;
+        }
+
+    }
+
+
+}
